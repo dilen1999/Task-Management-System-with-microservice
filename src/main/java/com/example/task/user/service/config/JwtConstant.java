b1321ef7361499a4b81de4c7a2a14b732309632d
Sahayanathan George Dilen <139237758+dilen1999@users.noreply.github.com>
2024-05-20 01:09:32 +0530

    first commit

---
+++ src/main/java/com/example/task/user/service/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.example.task.user.service.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY= "jwmakakmsakmkmxknkaslkamkndnasnkxk";
+    public static final String JWT_HEADER= "Authorization";
+}
---
+++ src/main/java/com/example/task/user/service/model/User.java
@@ -0,0 +1,22 @@
+package com.example.task.user.service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String password;
+    private String email;
+    private String role;
+    private String fullname;
+}
